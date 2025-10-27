96949224f166f6062035bdf453032f9a07b51cb7
Frida Rosenaa <macrosedk@gmail.com>
2025-10-27 14:02:26 +0100

    Add building

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,19 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true()
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
